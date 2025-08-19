dbc41ceaa74c4b67379e1d7002b1d96a71ca833a
Jordan Knee <j.knee@live.com>
2025-08-19 09:49:05 -0230

    adds concern to create a relationship to a person and their venues

---
+++ app/models/concerns/nl_venues/person.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module NlVenues
+  module Person
+    extend ActiveSupport::Concern
+
+    included do
+      def venue_communities
+        member_communities.where(type: 'VenueCommunity')
+      end
+
+      def venues
+        venue_communities.includes(:venue).map(&:venue)
+      end
+    end
+  end
+end
