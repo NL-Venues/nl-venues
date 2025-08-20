7f7fa093e3342e773904a89564522ec32d8e1ce7
Jordan Knee <j.knee@live.com>
2025-08-20 09:52:55 -0230

    person concern adds list of venues to its valid_event_hosts

--- app/models/concerns/nl_venues/person.rb
+++ app/models/concerns/nl_venues/person.rb
@@ -13,6 +13,10 @@ module NlVenues
       def venues
         venue_communities.includes(:venue).map(&:venue)
       end
+
+      def valid_event_host_ids
+        [id] + venues.pluck(:id)
+      end
     end
   end
 end
